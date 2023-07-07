f2cd6c5ce49f44c2f054a689ad55f13c5175ac3e
Samuel Shiau <shiausamuel@gmail.com>
2023-07-07 22:09:21 +0800

    added some new featires

---
+++ UNeedTickets/Controllers/CategoryController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UNeedTickets.Data;
+using UNeedTickets.Models;
+
+
+namespace UNeedTickets.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create() 
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj) 
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+		public IActionResult Edit(int id)
+		{
+            Category categoryFromDb= _db.Categories.Find(id);
+			return View(categoryFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			_db.Categories.Update(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			Category? categoryFromDb = _db.Categories.Find(id);
+			return View(categoryFromDb);
+		}
+		
+		[HttpPost, ActionName("Delete")]
+
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+	}
+}
